c7fdc92bbcdaa1165cfb6d6bde8e9449bd3b358a
Antonne Turner <antonneturner@gmail.com>
2021-01-21 14:22:35 -0500

    create server.js folder, create express variable, and server port, create public folder url path

---
+++ Develop/routes/api.js
@@ -0,0 +1,9 @@
+let router = require("express").Router()
+
+
+
+
+
+
+
+module.exports = router
---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+let express = require("express")
+let app = express()
+let port = process.env.PORT || 3000
+
+
+// makes public folder url path
+app.use(express.static("public"))
+// client -> Send data to the server  {
+//     note:"title",
+//     text:"this is A body"
+// }  ->  from  port the client data looks like: ksdjfhdskjhfsdkjhfkjdshfkjhskjfhdsklhfkhldslkfhdslkh
+
+
+// client data comes to the server-> ksdjfhdskjhfsdkjhfkjdshfkjhskjfhdsklhfkhldslkfhdslkh -> converts to original format {
+//     note:"title",
+// //     text:"this is A body"
+// }
+//data parsing - decrypting
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// put the clean and reader from original format to -  req.body, req.body will be used for your table or file when you create a new entry
+let api = require("./routes/api")
+let html = require("./routes/html")
+app.use(api)
+app.use(html)
+
+app.listen(port, function () {
+    console.log("app is listening on port " + port)
+})
