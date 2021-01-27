24feaeb03c1a09f26b1e3d7a73ca9bef2ce965ee
Antonne Turner <antonneturner@gmail.com>
2021-01-27 15:13:30 -0500

    create route for html, create post

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,9 +1,30 @@
 let router = require("express").Router()
+let db = require("../db/db.json")
+let fs = require("fs")
+
+router.get("/api/notes", function (req, res) {
+
+    res.json(db)
+
+})
 
 
+router.post("/api/notes", function (req, res) {
+
+    let notesData = req.body
+
+    db.push(notesData)
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+        res.json(db)
+    })
+
+    router.delete("/api/notes", function (req, res) {
+
+        let notesData = req.body
+
+        db
+
+    })
 
 
-
-
-
-module.exports = router
+    module.exports = router
